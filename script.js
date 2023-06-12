0ba83896b9a932d9b7db7dee495e6c997975a25a
Evgeniya Bystrikova <byseum@mail.ru>
2023-06-12 17:28:45 +0300

    Урок №2 работа с script.js: присвоены значения переменным, проработаны методы и свойства

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let screenPrice;
 let rollback; 
 let fullPrice;
 let adaptive; 
+let howMuch; 
+let howMuchDev; 
 
 title = "Переменные";
 screens = "Простые, Сложные, Интерактивные";
@@ -12,6 +14,16 @@ rollback = 78;
 fullPrice = 15000;
 adaptive = true; 
 
+howMuch = "Стоимость верстки экранов";
+roubles = "рублей";
+const NewStr = howMuch + " " + screenPrice + " " + roubles; 
+
+howMuchDev = "Стоимость разработки сайта";
+const NewStrDev = howMuchDev + " " + fullPrice + " " + roubles; 
+
+const percent = (fullPrice * (rollback/100)); 
+
+
 alert("Hello, It's me!");
 console.log("My name is Evgeniya!"); 
 
@@ -19,3 +31,8 @@ console.log(title);
 console.log(fullPrice);
 console.log(adaptive);
 console.log(screens.length);
+console.log(NewStr);
+console.log(NewStrDev);
+console.log(screens.toLocaleLowerCase());
+console.log(screens.split("ы"));
+console.log(percent);
