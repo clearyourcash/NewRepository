93000e04c5cae680e65a0db543c5329251a34fad
Evgeniya Bystrikova <byseum@mail.ru>
2023-06-12 16:14:04 +0300

    Урок №2: присвоены значения переменным

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ let rollback;
 let fullPrice;
 let adaptive; 
 
+title = "Переменные";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 10500;
+rollback = 78; 
+fullPrice = 15000;
+adaptive = true; 
+
 alert("Hello, It's me!");
 console.log("My name is Evgeniya!"); 
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
