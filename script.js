2d960bcacb65996348e0f160cbad7e69ac412fb9
Evgeniya Bystrikova <byseum@mail.ru>
2023-06-14 00:44:02 +0300

    Частично сделано задание по уроку №3 (п. 1-8)

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-let title;
-let screens;
-let screenPrice;
-let rollback; 
-let fullPrice;
-let adaptive; 
-let howMuch; 
-let howMuchDev; 
+'use strict;'
+let title = "Переменные";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 10500;
+let rollback = 78; 
+let fullPrice = 15000;
+let adaptive = true; 
 
-title = "Переменные";
-screens = "Простые, Сложные, Интерактивные";
-screenPrice = 10500;
-rollback = 78; 
-fullPrice = 15000;
-adaptive = true; 
-
-howMuch = "Стоимость верстки экранов";
-roubles = "рублей";
+let howMuch = "Стоимость верстки экранов";
+let roubles = "рублей";
 const NewStr = howMuch + " " + screenPrice + " " + roubles; 
 
-howMuchDev = "Стоимость разработки сайта";
+let howMuchDev = "Стоимость разработки сайта";
 const NewStrDev = howMuchDev + " " + fullPrice + " " + roubles; 
 
 const percent = (fullPrice * (rollback/100)); 
@@ -27,7 +19,7 @@ const percent = (fullPrice * (rollback/100));
 alert("Hello, It's me!");
 console.log("My name is Evgeniya!"); 
 
-console.log(title);
+//console.log(title);
 console.log(fullPrice);
 console.log(adaptive);
 console.log(screens.length);
@@ -36,3 +28,39 @@ console.log(NewStrDev);
 console.log(screens.toLocaleLowerCase());
 console.log(screens.split("ы"));
 console.log(percent);
+
+let title2 = prompt ("Как называется ваш проект?");
+title = title2; 
+console.log(title); 
+
+
+let screens2 = prompt ("Какие типы экранов нужно разработать?");
+screens = screens2;
+console.log(screens);
+
+
+let screenPrice2 = prompt ("Сколько будет стоить данная работа?");
+screenPrice = screenPrice2; 
+console.log (screenPrice);
+
+
+let adaptive2 = confirm ("Нужен ли адаптив на сайте?"); 
+if (adaptive2 == true) {
+  console.log ("Да")
+} else {
+  console.log ("Нет")
+}
+adaptive=adaptive2; 
+
+let service1 = prompt ("Какой дополнительный тип услуги нужен?");
+console.log(service1); 
+let servicePrice1 = prompt ("Сколько это будет стоить?");
+console.log(servicePrice1);
+let service2 = prompt ("Какой дополнительный тип услуги нужен?");
+console.log(service2); 
+let servicePrice2 = prompt ("Сколько это будет стоить?");
+console.log(servicePrice2);
+
+let fullPrice2 = parseInt(screenPrice) + parseInt(servicePrice1) + parseInt(servicePrice2); 
+fullPrice = fullPrice2; 
+console.log(fullPrice); 
